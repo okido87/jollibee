93ae19784b03384151206519768743711a4c04b0
Hien Nguyen <131453601+okido87@users.noreply.github.com>
2025-07-15 21:10:23 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+   const CONFIG = {
+            api: {
+                region: 'www',
+                appId: '9e857529-b31a-4452-9791-f3f10b95aaad',
+                accessKey: 'V2-hCB7E-rgYoG-x4o52-5DrGd-1tZ0k-QwoYJ-xksUy-BASob',
+                tableName: 'Report'
+            }
+        };
