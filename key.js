26d4284f712c8d984c0bf83f8ef3fb454e3aae9e
Hien Nguyen <131453601+okido87@users.noreply.github.com>
2025-07-22 12:17:19 +0700

    Update key.js

--- key.js
+++ key.js
@@ -4,7 +4,7 @@
                    region: 'www',
                 appId: '578c7c5a-2322-415f-a23d-22c8a3e0e254',
                 accessKey: 'V2-zsybA-5gDGD-N7N5o-lv18e-5SU84-7NWlc-gFEM3-1eilD',
-                tableName: 'Report'
+                tableName: 'Report' ,
                 appName : 'TRADEMARKETINGVISITREPORT-983265131'
             }
         };
