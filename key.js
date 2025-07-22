2eb047fbaca9f5eb9fd464668de5b869d92b709f
Hien Nguyen <131453601+okido87@users.noreply.github.com>
2025-07-22 12:14:12 +0700

    Update key.js

--- key.js
+++ key.js
@@ -1,10 +1,10 @@
 // Configuration , thay đổi app nên app name bị sai không hiển thị được ảnh
         const CONFIG = {
             api: {
-                region: 'www',
-                appId: '270166f8-4f6a-404f-a9f7-352e3a3c5f4e',
-                accessKey: 'V2-cM4K3-w10MC-051p1-YD6aO-IouDJ-l9Pvs-oCTxK-VmjYR',
-                tableName: 'Report' ,
+                   region: 'www',
+                appId: '578c7c5a-2322-415f-a23d-22c8a3e0e254',
+                accessKey: 'V2-zsybA-5gDGD-N7N5o-lv18e-5SU84-7NWlc-gFEM3-1eilD',
+                tableName: 'Report'
                 appName : 'TRADEMARKETINGVISITREPORT-983265131'
             }
         };
